de22d7bd0b6df4d2bc1b782d974266bd6c5a28c4
Dileep Jetty <42571672+JETTYDILEEP@users.noreply.github.com>
2019-05-15 20:07:16 +0530

    Update pg30.py

--- pg30.py
+++ pg30.py
@@ -1,10 +1,10 @@
 n=int(input())
 l=[int(x) for x in input().split()]
 li=[int(x) for x in input().split()]
-c=1
+a=1
 i=0
 for j in range(i+1,n):
     if(li[i]<=l[j]):
-        c+=1
+        a+=1
         i=j
-print(c)
+print(a)
